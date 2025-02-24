1655361da9ef06635e2d950c6aaf93b9cdcb37a8
kaoekb <kaoekb@gmail.com>
2025-02-24 22:55:12 +0300

    conf

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-BOT_TOKEN = os.getenv('Token_tg')
+BOT_TOKEN = int(os.getenv('Token_tg'))
 Token_MDB = os.getenv('db')
 MONGO_URI = f'mongodb+srv://{Token_MDB}@cluster0.w6k4v.mongodb.net/?retryWrites=true&w=majority'
 ADMIN_ID = int(os.getenv('Your_user_ID'))
