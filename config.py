82d8a7b03534dbc0fb24c370328cdd43ee47c712
kaoekb <kaoekb@gmail.com>
2025-04-11 01:35:30 +0300

    conf

--- config.py
+++ config.py
@@ -12,7 +12,8 @@ BOT_TOKEN = os.getenv('Token_tg')
 
 DB_USER = os.getenv('DB_USER')
 DB_PASS = os.getenv('DB_PASS')
+DB = os.getenv('DB')
 
-MONGO_URI = f'mongodb+srv://{DB_USER}:{DB_PASS}@cluster0.w6k4v.mongodb.net/?retryWrites=true&w=majority'
+MONGO_URI = f'mongodb+srv://{DB}@cluster0.w6k4v.mongodb.net/?retryWrites=true&w=majority'
 
 ADMIN_ID = int(os.getenv('Your_user_ID'))
