be13935b54b211923cba25af46b6ff245398b145
kaoekb <kaoekb@gmail.com>
2025-04-11 01:48:15 +0300

    db

--- bot/database.py
+++ bot/database.py
@@ -5,18 +5,26 @@ import logging
 import time
 from bot.utils import notify_admin
 
+# Настройка логгера с указанием времени
+logger = logging.getLogger(__name__)
+if not logger.hasHandlers():
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(levelname)s - %(message)s'
+    )
+
 def connect_to_mongo():
     while True:
         try:
             client = MongoClient(MONGO_URI, serverSelectionTimeoutMS=5000)
-            client.server_info()  # Проверка подключения
-            logging.info("✅ Успешное подключение к MongoDB")
+            client.server_info()
+            logger.info("✅ Успешное подключение к MongoDB")
             return client
         except errors.ServerSelectionTimeoutError:
-            logging.error("❌ Ошибка подключения к MongoDB. Повторная попытка через 5 секунд...")
+            logger.error("❌ Ошибка подключения к MongoDB. Повторная попытка через 5 секунд...")
             time.sleep(5)
         except Exception as e:
-            logging.critical(f"❌ Критическая ошибка MongoDB: {e}")
+            logger.critical(f"❌ Критическая ошибка MongoDB: {e}")
             notify_admin(f"❌ Критическая ошибка подключения к MongoDB: {e}")
             time.sleep(5)
 
--- config.py
+++ config.py
@@ -5,11 +5,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 BOT_TOKEN = os.getenv('Token_tg')
-# Token_MDB = os.getenv('db')
-# Token_MDB = urllib.parse.quote_plus(Token_MDB)  # Кодируем символы в пароле
-
-# MONGO_URI = f'mongodb+srv://{Token_MDB}@cluster0.w6k4v.mongodb.net/?retryWrites=true&w=majority'
-
 DB_USER = os.getenv('DB_USER')
 DB_PASS = os.getenv('DB_PASS')
 DB = os.getenv('DB')
