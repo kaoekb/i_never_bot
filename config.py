d2de05c9b4f2974b0077e5c7fc4d394af23b838e
kaoekb <kaoekb@gmail.com>
2025-04-11 03:39:23 +0300

    up

--- config.py
+++ config.py
@@ -11,8 +11,7 @@ DB_USER = os.getenv('DB_USER')
 DB_PASS = os.getenv('DB_PASS')
 # db = os.getenv("Token_MDB")
 
-MONGO_URI = f'mongodb+srv://Anton:12345@cluster0.w6k4v.mongodb.net/?retryWrites=true&w=majority'
-
+MONGO_URI = 'mongodb+srv://Anton:12345@cluster0.w6k4v.mongodb.net/?retryWrites=true&w=majority'
 ADMIN_ID = int(os.getenv('Your_user_ID'))
 
 logging.info(f"[DEBUG] MONGO_URI: {MONGO_URI}")
