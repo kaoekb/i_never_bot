7d29889ecfc324db8c0f74055d73df01e15ca271
kaoekb <kaoekb@gmail.com>
2025-04-11 03:21:16 +0300

    up

--- config.py
+++ config.py
@@ -2,14 +2,14 @@
 import os
 import urllib
 from dotenv import load_dotenv
-from urllib.parse import quote_plus
+# from urllib.parse import quote_plus
 import logging
 
 load_dotenv()
 BOT_TOKEN = os.getenv('Token_tg')
 DB_USER = os.getenv('DB_USER')
 DB_PASS = os.getenv('DB_PASS')
-Token_MDB = quote_plus(os.getenv("Token_MDB"))
+Token_MDB = os.getenv("Token_MDB")
 
 MONGO_URI = f'mongodb+srv://{Token_MDB}@cluster0.w6k4v.mongodb.net/?retryWrites=true&w=majority'
 
